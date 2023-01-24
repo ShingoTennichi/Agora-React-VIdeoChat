9e0bbe416e9c9a00cb3992874415ab37489d0fd7
ShingoTennichi <sngsng.ttt@gmail.com>
2023-01-23 16:04:32 -0800

    simple video chat app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// 3987f202c7514bb3a8a1e6becc39e77e
+import { useState } from "react";
+
+import AgoraUIKit from "agora-react-uikit";
+import styles from './App.module.css';
+
+type rtcProps = {
+    appId: string,
+    channel: string,
+    token: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [videoCall, setVideoCall] = useState<boolean>(false);
+    const rtcProps: rtcProps = {
+        appId: process.env.REACT_APP_ID ?? "",
+        channel: "test",
+        token: process.env.REACT_APP_TOKEN ?? ""
+    }
+
+    const callbacks = {
+        EndCall: () => setVideoCall(false)
+    }
+
+    // async function startVideoCall() {
+    //     rtcProps.token = await fetch('http://localhost:8080/')
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //         return data;
+    //     });
+    // }
+
+    console.log(rtcProps);
+    return videoCall ? (
+        <div className={styles.container}>
+            <nav className={styles.nav}>
+                <h1>Video Chat</h1>
+            </nav>
+            <div className={styles.container}>
+                <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+            </div>
+        </div>
+    ) :
+        (
+            <div className={styles.container}>
+                <nav className={styles.nav}>
+                    <h1>Video Chat</h1>
+                    <div>
+                        <button onClick={() => setVideoCall(true)}>Start Video Call</button>
+                        {/* <button onClick={() => startVideoCall()}>Start Video Call</button> */}
+                    </div>
+                </nav>
+            </div>
+        )
 }
 
 export default App;
